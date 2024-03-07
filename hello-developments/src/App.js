0c5adfea3c12be69140d4e9256e5b4bb21dc4cde
shaique akhtar <98828131+shaiqueakhtar@users.noreply.github.com>
2024-03-07 18:31:13 +0530

    1st commit
    azim changes

---
+++ hello-developments/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+import Main from './Component/Main';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    
+    
+    </div>
+
+  );
+}
+
+export default App;
